f80e80041eac568d17a5546890b8a5fd5b48c524
stickyingers <2102320695@qq.com>
2022-10-13 22:23:52 +0800

    reconciliation

--- Didact.js
+++ Didact.js
@@ -29,27 +29,50 @@ function createTextElement(text) {
 function createDOM(fiber) {
   const dom =
     fiber.type === "TEXT_ELEMENT"
-      ? document.createTextNode(element.props.nodeValue)
+      ? document.createTextNode(fiber.props.nodeValue)
       : document.createElement(fiber.type);
   //object.keys返回一个包含对象所有键的数组,将element的props传入
   //const isProperty =key => key!=='children'
-  Object.keys(element)
+  Object.keys(fiber)
     .filter((item) => item !== "children")
     .forEach((prop) => (dom[prop] = fiber.props[prop]));
 }
 
+function commitRoot(){
+  //add nodes to dom
+  deletions.forEach(commitWork)
+  commitWork(wipRoot.child)
+  wipRoot=null
+}
+
+function commitWork(fiber){
+  if(!fiber){
+    return 
+  }
+  const domParent=fiber.parent.dom
+  domParent.appendChild(fiber.dom)
+  commitWork(fiber.child)
+  commitWork(fiber.sibling)
+}
+
 function render(element,container){
-    nextUnitOfWork={
+    wipRoot={
         dom: container,
         props:{
             children:[element]
-        }
+        },
+        alternate: currentRoot,
     }
+    deletions=[]//保存要删除的fiber
+    nextUnitOfWork=wipRoot//work-in-progress树，防止为渲染完的app出现在屏幕上
 }
 //递归导致问题，将DOM树分成小部分异步执行
 //并发模式
 //这里是react中的reconciler
 let nextUnitOfWork =null
+let wipRoot=null 
+let currentRoot=null
+let deletions=null
 function workloop(deadline) {
     let shouldYield =false
     while(nextUnitOfWork && !shouldYield){
@@ -57,6 +80,9 @@ function workloop(deadline) {
             nextUnitOfWork
         )//根据顺序执行一个个unit的work，在执行这一个unit的同时返回下一个unit
         shouldYield=deadline.timeRemaining() <1//在剩余时间不足时将控制权交给浏览器
+    }
+    if(!nextUnitOfWork&&wipRoot){
+      commitRoot()
     }
     requestIdleCallback(workloop)//控制权返回浏览器，进入等待的状态
 }
@@ -70,30 +96,11 @@ function performUnitOfWork(fiber) {
   if (!fiber.dom) {
     fiber.dom = createDOM(fiber);
   }
-  if (fiber.parent) {
-    fiber.parent.dom.appendChild(fiber.dom);
-  }
+ 
   //为除了根节点之外的fiber创造节点,并将该节点加到父节点的dom（创建的dom节点）上
   //create new fiber;
   const elements = fiber.props.children;
-  let index = 0;
-  let prevSibling = null;
-  while (index < elements.length) {
-    const element = elements[index];
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null,
-    };
-    if (index === 0) {
-      fiber.child = newFiber;
-    } else {
-      prevSibling.sibling = newFiber;
-    }
-    prevSibling = newFiber;
-    index++;
-  }
+  reconcileChildren(fiber,elements)
   //为所有子节点设置fiber,并为之设置parent，child，sibling属性
   //return next unit of work
   if (fiber.child) {
@@ -107,10 +114,66 @@ function performUnitOfWork(fiber) {
     nextFiber = nextFiber.parent;//都没有的话向上遍历各个uncle节点直到根节点
   }
 }
+//这个函数用来对比新旧fiber树的差别
+function reconcileChildren(wipFiber,elements){
+  let index = 0;
+  let oldFiber=wipFiber.alternate&&wipFiber.alternate.child
+  let prevSibling = null;
+
+  while(index<elements.length||oldFiber!=null){
+    const element=elements[index]
+    let newFiber=null
+    const sameType=oldFiber&&element&&element.type==oldFiber.type
+    if(sameType){
+      //更新节点的属性
+      newFiber={
+        type:oldFiber.type,
+        props:element.props,
+        dom:oldFiber.dom,
+        parent:wipFiber,
+        alternate:oldFiber,
+        effectTag:"UPDATE"//告诉renderer如何处理
+      }
+    }
+    if(element&&!sameType){
+      //增加节点
+      newFiber={
+        type:element.type,
+        props:element.props,
+        dom:null,//WTF is dom???????!!!!!!
+        parent:wipFiber,
+        alternate:null,
+        effectTag:"PLACEMENT"
+      }
+    }
+    if(oldFiber&&!sameType){
+      //删除该节点
+      oldFiber.effectTag="DELETION"
+      deletions.push(oldFiber)
+    }
+
+  }
+    while (index < elements.length) {
+      const element = elements[index];
+      const newFiber = {
+        type: element.type,
+        props: element.props,
+        parent: fiber,
+        dom: null,
+      };
+      if (index === 0) {
+        fiber.child = newFiber;
+      } else {
+        prevSibling.sibling = newFiber;
+      }
+      prevSibling = newFiber;
+      index++;
+    }
+}
 
 
 //每一个element就是一个fiber，每一个fiber就是一个任务单元
-
+//reconcilation
 
 
 
--- react理念.md
+++ react理念.md
@@ -3,6 +3,10 @@
 >我们认为，React 是用 JavaScript 构建**快速响应**的大型 Web 应用程序的首选方式。它在 Facebook 和 Instagram 上表现优秀
 
 **同步更新——>异步可中断的更新**
+**rendering a React app is akin to calling a function whose body contains calls to other functions**
+**旧的react使用递归的方式，可能会掉帧并且使动画不连贯**
+
+[参考文章](https://github.com/acdlite/react-fiber-architecture)
 
 ## react架构
 
@@ -12,9 +16,16 @@
 
 ![Image](react更新.jpg)
 
-### reconciler
+## schelduler
+
+We can prioritize work coming from **user interactions** (such as an animation caused by a button click) over less important background work (such as rendering new content just loaded from the network) to avoid dropping frames
+
+## reconciler
 
 内部采用了fiber的数据结构
+
+是核心组件
+>This separation means that React DOM and React Native can use their own renderers while sharing the same reconciler, provided by React core.
 
 * class 组件
 * props，state
@@ -23,18 +34,29 @@
 * react.lazy,错误边界
 * concurrent mode，suspense
 
-##### fiber
+>对比DOM树和fiber树，看哪些部分需要re-render
+
+>React使用 key 这个属性来优化 reconciliation 过程。比如, key 属性可以用来检测 elements 数组中的子组件是否仅仅是更换了位置
+
+#### fiber
 
 ![Image](fiber.png)
 
 >reactDOM=fiber
 >fiber(纤程)，是协程的一种
 >程序执行过程：协程，线程，进程；generator是js协程的实现
+>A fiber represents a unit of work
+>virtual stack frame
 
-除了在DOM中工作，可以在别的环境中工作
+具体：**a fiber is a JavaScript object that contains information about a component, its input, and its output**
 
-### renderer
+
+## renderer
 
 * built-in组件(div，span...)
 
 可以在别的环境中工作（plugin）
+
+## 虚拟DOM
+
+when you render a React application, a tree of nodes that **describes the app** is generated and saved in memory.This tree is then flushed to the rendering environment , it's translated to a set of **DOM operations**. When the app is updated (usually via setState), a new tree is generated. The new tree is **diffed with the previous** tree to compute which operations are needed to update the rendered app
