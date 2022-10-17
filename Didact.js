cce0583f8170966d2b2e7bea6ed8b1b9870627e8
stickyingers <2102320695@qq.com>
2022-10-17 21:37:45 +0800

    完成

--- Didact.js
+++ Didact.js
@@ -11,7 +11,6 @@ function createELement(type,props,...children){
             typeof children===Object ? child : createTextElement(child)
            })
         }
-
     }
 }
 //使用剩余参数，使children成为数组
@@ -80,7 +79,11 @@ function commitWork(fiber){
   if(!fiber){
     return 
   }
-  const domParent=fiber.parent.dom
+  let domParentFiber=fiber.parent
+  while(!domParentFiber.dom){
+    domParentFiber=domParentFiber.parent
+  }
+  const domParent=domParentFiber.dom
   if(fiber.effectTag==="PLACEMENT"&&fiber.dom!=null){
     domParent.appendChild(fiber.dom)
   }
@@ -94,7 +97,14 @@ function commitWork(fiber){
   commitWork(fiber.child)
   commitWork(fiber.sibling)
 }
-
+function commitDeletion(fiber,domParent){
+  if(fiber.dom){
+    domParent.reconcileChildren(fiber.dom)
+  }
+  else{
+    commitDeletion(fiber.child,domParent)
+  }
+}
 function render(element,container){
     wipRoot={
         dom: container,
@@ -132,15 +142,15 @@ requestIdleCallback(workloop)//在浏览器有空闲时间时执行workloop
 //mdn文档：插入一个函数，这个函数将在浏览器空闲时期被调用。
 //这使开发者能够在主事件循环上执行后台和低优先级工作，而不会影响延迟关键事件，如动画和输入响应。
 function performUnitOfWork(fiber) {
-  //add dom node;
-  if (!fiber.dom) {
-    fiber.dom = createDOM(fiber);
+  const isFunctionComponent=fiber.type instanceof Function
+  if(isFunctionComponent){
+    updateFunctionComponent(fiber)
   }
- 
-  //为除了根节点之外的fiber创造节点,并将该节点加到父节点的dom（创建的dom节点）上
-  //create new fiber;
-  const elements = fiber.props.children;
-  reconcileChildren(fiber,elements)
+  else{
+    updateHostComponent(fiber)
+  }
+//判断是否是函数组件
+  
   //为所有子节点设置fiber,并为之设置parent，child，sibling属性
   //return next unit of work
   if (fiber.child) {
@@ -153,6 +163,54 @@ function performUnitOfWork(fiber) {
     }
     nextFiber = nextFiber.parent;//都没有的话向上遍历各个uncle节点直到根节点
   }
+
+}
+function updateFunctionComponent(fiber){
+  wipFiber=fiber
+  hookIndex=0
+  wipFiber.hooks=[]
+  const children=[fiber.type(fiber.props)]
+reconcileChildren(fiber.children)
+}
+
+//状态钩子
+let wipFiber=null
+let hookIndex=null
+function useState(initial){
+  oldHook=wipFiber.alternate&&wipFiber.alternate.hooks&&
+  wipFiber.alternate.hooks[hookIndex]
+  const hook={
+    state: oldHook?oldHook.state:initial,
+    queue:[]
+  }
+  //检查有无旧hooks
+  const setState=action=>{
+    hook.queue.push(action)
+    wipRoot={
+      dom: currentRoot.dom,
+      props:currentRoot.props,
+      alternate:currentRoot
+    }
+    nextUnitOfWork=wipRoot
+    deletions=[]
+  }
+  //返回一个会引起re-render的函数
+  const actions=oldHook?oldHook.queue:[]
+  actions.forEach(action=>{
+    hook.state=action(hook.state)
+  })
+  //运行actions
+  wipFiber.hooks.push(hook)
+  hookIndex++
+  return[hook.state,setState]
+}
+
+
+function updateHostComponent(fiber){
+  if(!fiber.dom){
+    fiber.dom=createDOM(fiber)
+  }
+  reconcileChildren(fiber,fiber.props.children)
 }
 //这个函数用来对比新旧fiber树的差别
 function reconcileChildren(wipFiber,elements){
